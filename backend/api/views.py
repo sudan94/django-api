59fa7cb33d902bd8c59d54a309ce7fdcac3ccf6f
unknown <sudanupadhaya@gmail.com>
2022-05-18 22:19:55 +0200

    basics done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request,*args,**kwargs):
-    return JsonResponse({'message':'Hello World'})
+    # print(dir(request))
+    # request.body
+    body = request.body # byte string of json b
+    data = {}
+    try: 
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers) # dict used casues json cannot prases this data directly
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
