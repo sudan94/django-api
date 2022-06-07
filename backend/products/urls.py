2f2c45f020c120548e0c6235adde91073ca8c1d1
sudan94 <sudanupadhaya@gmail.com>
2022-06-07 11:13:27 +0200

    listcreateAPIview for list and create at the same time

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/',views.ProductDetailAPIView.as_view())
+    path('<int:pk>/',views.ProductDetailAPIView.as_view()),
+    path('create/',views.product_create_view),
+    path('',views.product_list_create_view)
+
+
 ]
 
 
--- backend/products/views.py
+++ backend/products/views.py
@@ -10,3 +10,28 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 # look up fild pk
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+    def perform_create(self,serializer):
+        title = serializer.validated_data.get('title')
+        description = serializer.validated_data.get('description')
+        # or None
+        if description is None:
+            description = title
+        serializer.save(description=description)
+        # can use signals in here didnot get much in this part perfrom_create
+product_create_view = ProductCreateAPIView.as_view()
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+"""
+We can use list view instead of creat view as it will list and also allows creation endpoint 
+"""
